85fd18a9c728d46b2297123f0b3298d103a739f1
Bill Reed <billnreed@gmail.com>
2020-11-09 13:58:21 -0500

    slot icons

--- hades-boon-explorer/scenes/Boon.gd
+++ hades-boon-explorer/scenes/Boon.gd
@@ -5,9 +5,6 @@ class_name Boon
 
 export(Resource) var boon_data setget _set_boon_data
 
-onready var sprite = $Sprite
-onready var titleLabel = $Title
-
 func _set_boon_data(value) -> void:
     boon_data = value
 
@@ -15,3 +12,14 @@ func _set_boon_data(value) -> void:
         $Sprite.texture = value.icon
     if $Title:
         $Title.text = value.title
+
+    if $Slots/Attack:
+        $Slots/Attack.visible = boon_data.attack
+    if $Slots/Special:
+        $Slots/Special.visible = boon_data.special
+    if $Slots/Cast:
+        $Slots/Cast.visible = boon_data.cast
+    if $Slots/Dash:
+        $Slots/Dash.visible = boon_data.dash
+    if $Slots/Call:
+        $Slots/Call.visible = boon_data.call
--- hades-boon-explorer/scripts/BoonData.gd
+++ hades-boon-explorer/scripts/BoonData.gd
@@ -6,3 +6,9 @@ export(String) var id
 export(String) var title
 export(Texture) var icon
 export(Array, Array, String) var dependencies
+
+export(bool) var attack
+export(bool) var special
+export(bool) var cast
+export(bool) var dash
+export(bool) var call

48a2e69ebc7314e8eb885e185c8e6288efeb81d7
Bill Reed <billnreed@gmail.com>
2021-02-05 15:37:55 -0500

    alpha cutoff shader + gui

---
+++ alpha-cutoff-shader/World.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+const TRANSITION_SOURCE_DIRECTORY = "res://transitions/"
+
+var transition_sources = []
+
+onready var sprite = $Sprite
+onready var animation_player = $Sprite/AnimationPlayer
+onready var button_container = $ColorRect/VBoxContainer
+
+func _init() -> void:
+    var directory = Directory.new()
+    if (directory.open(TRANSITION_SOURCE_DIRECTORY) == OK):
+        directory.list_dir_begin()
+        var file_name = directory.get_next()
+        while file_name != "":
+            if not file_name.ends_with(".import") and not file_name == "." and not file_name == "..":
+                transition_sources.append(load(TRANSITION_SOURCE_DIRECTORY + file_name))
+            file_name = directory.get_next()
+
+func _ready() -> void:
+    animation_player.current_animation = "transition"
+    animation_player.stop()
+
+    for i in range(0, transition_sources.size()):
+        var button = Button.new()
+        button.text = "Transition %s" % i
+        button.connect("pressed", self, "_on_Button_pressed", [i])
+        button_container.add_child(button)
+
+        var icon = Sprite.new()
+        icon.texture = transition_sources[i]
+        icon.scale = Vector2(20, 20) / transition_sources[i].get_size()
+        button.add_child(icon)
+        icon.position = Vector2(10, 10)
+
+
+func _on_Button_pressed(transition_index: int) -> void:
+    _play_transition_with_texture(transition_sources[transition_index])
+
+
+func _on_RandomNoiseButton_pressed() -> void:
+    var noise = OpenSimplexNoise.new()
+    noise.seed = randi()
+
+    var image = noise.get_seamless_image(200)
+    var texture = ImageTexture.new()
+    texture.create_from_image(image)
+
+    _play_transition_with_texture(texture)
+
+func _play_transition_with_texture(texture) -> void:
+    animation_player.stop()
+    sprite.material.set_shader_param("source", texture)
+    animation_player.play()
